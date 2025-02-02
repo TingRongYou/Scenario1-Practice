94ddc1eba685546c6d820163cd7f4faf80f92b68
Ting Rong You <ryting999@gmail.com>
2025-02-02 21:03:40 +0800

    Mad Libs Game

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,21 +5,35 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		//MAD LIBS GAME
+		
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.print("Enter your name: ");
-		String name = scanner.nextLine();
+		String adjective1;
+		String noun1;
+		String adjective2;
+		String verb1;
+		String adjective3;
 		
-		System.out.print("Enter your age: ");
-		int age = scanner.nextInt();
+		System.out.print("Enter an adjective (description): ");
+		adjective1 = scanner.nextLine();
+		System.out.print("Enter a noun (animal or person): ");
+		noun1 = scanner.nextLine();
+		System.out.print("Enter an adjective (description): ");
+		adjective2 = scanner.nextLine();	
+		System.out.print("Enter a verb end with -ing (action): ");
+		verb1 = scanner.nextLine();
+		System.out.print("Enter an adjective (description): ");
+		adjective3 = scanner.nextLine();
+
 		
-		System.out.println("Hello " + name);
-		System.out.println("You are " + age + " years old");
+		System.out.println("\nToday I went to a " + adjective1 + " zoo.");
+		System.out.println("In an exhibit, I saw a " + noun1 + ".");
+		System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
+		System.out.println("I was " + adjective3 + "!");
 		
-		System.out.println("Hello World");
-				
 		scanner.close();
-		
+
 	}
 
 }
