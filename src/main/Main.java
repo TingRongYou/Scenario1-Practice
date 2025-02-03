fe96732a33437fc883ea538a34097db43ed86406
Ting Rong You <ryting999@gmail.com>
2025-02-03 12:37:46 +0800

    Compound Interest Calculator

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,23 +5,33 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//printf() = is a method used to format output
+		//COMPOUND INTEREST CALCULATOR
 		
-		//%[flags][width][.precision][specifier-character]
+		Scanner scanner = new Scanner(System.in);
 		
-		String name = "Spongebob";
-		char firstLetter = 'S';
-		int age = 30;
-		double height = 60.5;
-		boolean isEmployed = true;
+		double principal;
+		double rate;
+		int timesCompounded;
+		int years;
+		double amount;
 		
-		System.out.printf("Hello %s\n", name);
-		System.out.printf("Your name starts with a %c\n", firstLetter);
-		System.out.printf("You are %d years old\n", age);
-		System.out.printf("You are %f inches tall\n", height);
-		System.out.printf("Employed: %b\n", isEmployed);
+		System.out.print("Enter the principal amount: ");
+		principal = scanner.nextDouble();
 		
-		System.out.printf("%s is %d years old", name, age);
+		System.out.print("Enter the interest rate (in %): ");
+		rate = scanner.nextDouble() / 100;
+		
+		System.out.print("Enter the # of times compounded per year: ");
+		timesCompounded = scanner.nextInt();
+		
+		System.out.print("Enter the # of years: ");
+		years = scanner.nextInt();
+		
+		amount = principal * Math.pow(1 + rate / timesCompounded, timesCompounded * years);
+		
+		System.out.printf("The amount after %d years is $%.2f", years, amount);
+		
+		scanner.close();
 
 	}
 
