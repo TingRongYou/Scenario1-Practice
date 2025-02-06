196c61357ec359d9541ab6df5f4a0211de8050b2
Ting Rong You <ryting999@gmail.com>
2025-02-06 19:11:00 +0800

    Enhanced Switches

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,48 +5,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//WEIGHT CONVERSION PROGRAM
-		
-		//Declare variables
-		
+		//ENHANCED SWITCH = A replacement to many else if statements(Java 14 feature)
+
 		Scanner scanner = new Scanner(System.in);
 		
-		double weight;
-		double newWeight;
-		int choice;
+		System.out.print("Enter the day of the week: ");
+		String day = scanner.nextLine();
 		
-		//Welcome message
-		
-		System.out.println("Weight Conversion Program");
-		System.out.println("1: Convert lbs to kgs");
-		System.out.println("2: Convert kgs to lbs");
-		
-		//Prompt for user choice
-		
-		System.out.print("Choose an option: ");
-		choice = scanner.nextInt();
-		
-		//Option 1 convert lbs to kgs
-		//Option 2 convert kgs to lbs
-		//else print not a valid choice
-		
-		if(choice == 1) {
-			System.out.print("Enter the weight in lbs: ");
-			weight = scanner.nextDouble();
-			newWeight = weight * 0.453592;
-			System.out.printf("The new weight in kgs is: %.2f", newWeight);
+		switch(day) {
+			case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" -> 
+				System.out.println("It is a weekday");
+			case "Saturday", "Sunday" -> 
+				System.out.println("It is the weekend");
+			default -> System.out.println(day + " is not a day");
 		}
-		else if(choice == 2) {
-			System.out.print("Enter the weight in kgs: ");
-			weight = scanner.nextDouble();
-			newWeight = weight * 2.20462;
-			System.out.printf("The new weight in lbs is: %.2f", newWeight);
-		}
-		else {
-			System.out.println("That was not a valid choice");
-		}
-		
-		scanner.close();
 		
 	}
 
