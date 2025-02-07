f212a18dfd9bc967677bec0110757976e4cf4b7e
Ting Rong You <ryting999@gmail.com>
2025-02-07 21:20:32 +0800

    Constructors version 1

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,12 +5,28 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//OBJECT = An entity that holds data(attributes) and can perform actions(method). It is a reference data type
+		//CONSTRUCTOR = A special method to initialize objects
+		//You can pass arguments to a constructor
+		//And set up initial values
 		
-		Car car = new Car();
+		Student student1 = new Student("Spongebob", 30, 3.2);
+		Student student2 = new Student("Patrick", 34, 1.5);
+		Student student3 = new Student("Sandy", 27, 4.0);
 		
-		car.drive();
-		car.brake();
+		System.out.println(student1.name);
+		System.out.println(student1.age);
+		System.out.println(student1.gpa);
+		System.out.println(student1.isEnrolled);
+		
+		System.out.println(student2.name);
+		System.out.println(student2.age);
+		System.out.println(student2.gpa);
+		System.out.println(student2.isEnrolled);
+		
+		System.out.println(student3.name);
+		System.out.println(student3.age);
+		System.out.println(student3.gpa);
+		System.out.println(student3.isEnrolled);
 				
 	}
 	
