c94e7ea667f78f1fd506d500865a5bce453f871e
Ting Rong You <ryting999@gmail.com>
2025-02-07 21:22:50 +0800

    Constructors version 2

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,20 +13,9 @@ public class Main {
 		Student student2 = new Student("Patrick", 34, 1.5);
 		Student student3 = new Student("Sandy", 27, 4.0);
 		
-		System.out.println(student1.name);
-		System.out.println(student1.age);
-		System.out.println(student1.gpa);
-		System.out.println(student1.isEnrolled);
-		
-		System.out.println(student2.name);
-		System.out.println(student2.age);
-		System.out.println(student2.gpa);
-		System.out.println(student2.isEnrolled);
-		
-		System.out.println(student3.name);
-		System.out.println(student3.age);
-		System.out.println(student3.gpa);
-		System.out.println(student3.isEnrolled);
+		student1.study();
+		student2.study();
+		student3.study();
 				
 	}
 	
