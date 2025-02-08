5a3aba403cfb7a1abecbfe2c637b1c8ba4a14f13
Ting Rong You <ryting999@gmail.com>
2025-02-08 20:11:28 +0800

    Alarm Clock

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,139 +1,40 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Random;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		//JAVA HANGMAN GAME
+		//JAVA ALARM CLOCK
 		
-		String filePath = "src\\words.txt";
-		ArrayList<String> words = new ArrayList<>();
-		
-		try(BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-			String line;
-			while((line = reader.readLine()) != null) {
-				words.add(line.trim());
-			}
-		}
-		catch(FileNotFoundException e) {
-			System.out.println("Could not find file");
-		}
-		catch(IOException e) {
-			System.out.println("Something went wrong");
-		}
-		
-		Random random = new Random();
-		
-		String word = words.get(random.nextInt(words.size()));
-				
 		Scanner scanner = new Scanner(System.in);
-		ArrayList<Character> wordState = new ArrayList<>();
-		int wrongGuesses = 0;
 		
-		for(int i = 0; i < word.length(); i++) {
-			wordState.add('_');
-		}
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+		LocalTime alarmTime = null;
+		String filePath = "src\\see-you-later-203103.wav";
 		
-		System.out.println("************************");
-		System.out.println("Welcome to Java Hangman!");
-		System.out.println("************************");
-		
-		while(wrongGuesses < 6) {
-			
-			System.out.println(getHangmanArt(wrongGuesses));
-			
-			System.out.print("Word: ");
-			
-			for(char c : wordState) {
-				System.out.print(c + " ");
-			}
-			
-			System.out.println();
-			
-			System.out.print("Guess a letter: ");
-			char guess = scanner.next().toLowerCase().charAt(0);
-			
-			if(word.indexOf(guess) >= 0) {
-				System.out.println("Correct guess!");
+		while(alarmTime == null) {
+			try {
+				System.out.print("Enter an alarm time (HH:MM:SS): ");
+				String inputTime = scanner.nextLine();
 				
-				for(int i = 0; i < word.length(); i++) {
-					if(word.charAt(i) == guess) {
-						wordState.set(i, guess);
-					}
-				}
-				
-				if(!wordState.contains('_')) {
-					System.out.println(getHangmanArt(wrongGuesses));
-					System.out.println("YOU WIN!");
-					System.out.println("The word was: " + word);
-					break;
-				}
+				alarmTime = LocalTime.parse(inputTime, formatter);
+				System.out.println("Alarm set for " + alarmTime);
 				
 			}
-			else {
-				wrongGuesses++;
-				System.out.println("Wrong guess!");
+			catch(DateTimeParseException e) {
+				System.out.println("Invalid format. Please use HH:MM:SS");
 			}
 		}
 		
-		if(wrongGuesses >= 6) {
-			System.out.println(getHangmanArt(wrongGuesses));
-			System.out.println("GAME OVER!");
-			System.out.println("The word was: " + word);
-		}
-		
-		scanner.close(); 
-				
+		AlarmClock alarmClock = new AlarmClock(alarmTime, filePath, scanner);
+		Thread alarmThread = new Thread(alarmClock);
+		alarmThread.start();
+					
 	}
-	
-	static String getHangmanArt(int wrongGuesses) {
 		
-		return switch(wrongGuesses) {
-		case 0 -> """
-				  
-				  
-				  
-				  """;
-		case 1 -> """
-				   o
-		  
-		  
-				  """;
-		case 2 -> """
-		           o
-		           |
-		  
-		          """;
-		case 3 -> """
-				   o
-				  /|
-
-				  """;
-		case 4 -> """
-				   o
-				  /|\\
-
-				  """;
-		case 5 -> """
-				   o
-				  /|\\
-				  /
-				  """;
-		case 6 -> """
-				   o
-				  /|\\
-				  / \\
-				  """;
-		default -> "";
-		};
-	}
-	
 }
